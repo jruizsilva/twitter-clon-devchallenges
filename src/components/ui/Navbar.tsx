eb5a486002356ca1179749492aa3ede66173e458
jruizsilva <ruizsilvajonathan@gmail.com>
2023-04-21 03:20:16 -0300

    header initial design

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,13 +3,12 @@ import {
   Divider,
   HStack,
   Heading,
+  Icon,
   Image,
   Popover,
-  PopoverArrow,
   PopoverContent,
   PopoverTrigger,
-  Stack,
-  VStack
+  Stack
 } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { AiFillCaretDown } from 'react-icons/ai'
@@ -21,43 +20,60 @@ interface Props {}
 export function Navbar(props: Props) {
   return (
     <>
-      <Stack
-        as='header'
-        flexDirection='row'
-        flexWrap='nowrap'
-        height='68px'
-        justifyContent='space-between'
-        padding='20px 16px'
-        width='100%'
-      >
-        <Box>
-          <Image alt='tweeter small' src={tweeterSmall} width='41px' />
-        </Box>
-        <Box>
-          <Popover placement='bottom-end'>
-            <PopoverTrigger>
-              <HStack>
-                <Image
-                  alt='tweeter small'
-                  borderRadius='8px'
-                  src={`https://i.pravatar.cc/50`}
-                  width='41px'
-                />
-                <Heading size='xs' userSelect='none'>
-                  Xanthe Neal
-                </Heading>
-                <AiFillCaretDown />
-              </HStack>
-            </PopoverTrigger>
-            <PopoverContent borderRadius='12px' padding='14px' width='192px'>
-              <Link to='/profile/1'>My profile</Link>
-              <Link to='/'>Group Chat</Link>
-              <Link to='/'>Settings</Link>
-              <Divider />
-              <Link to='/'>Logout</Link>
-            </PopoverContent>
-          </Popover>
-        </Box>
+      <Stack as='header' padding='20px 16px'>
+        <HStack justifyContent='space-between'>
+          <HStack>
+            <Image alt='tweeter small' src={tweeterSmall} width='41px' />
+            <Heading size='md'>Tweeter</Heading>
+          </HStack>
+          <HStack
+            as='nav'
+            display={{ base: 'none', md: 'flex' }}
+            spacing={{ base: '0', md: '32px', lg: '64px', xl: '80px' }}
+          >
+            <Heading size='xs'>Home</Heading>
+            <Heading size='xs'>Explore</Heading>
+            <Heading size='xs'>Bookmarks</Heading>
+          </HStack>
+          <Box>
+            <Popover placement='bottom-end'>
+              <PopoverTrigger>
+                <HStack>
+                  <Image
+                    alt='tweeter small'
+                    borderRadius='8px'
+                    src={`https://i.pravatar.cc/50`}
+                    width='36px'
+                  />
+                  <Heading
+                    display={{
+                      base: 'none',
+                      md: 'block'
+                    }}
+                    size='xs'
+                    userSelect='none'
+                  >
+                    Xanthe Neal
+                  </Heading>
+                  <Icon
+                    as={AiFillCaretDown}
+                    display={{
+                      base: 'none',
+                      md: 'block'
+                    }}
+                  />
+                </HStack>
+              </PopoverTrigger>
+              <PopoverContent borderRadius='12px' padding='14px' width='192px'>
+                <Link to='/profile/1'>My profile</Link>
+                <Link to='/'>Group Chat</Link>
+                <Link to='/'>Settings</Link>
+                <Divider />
+                <Link to='/'>Logout</Link>
+              </PopoverContent>
+            </Popover>
+          </Box>
+        </HStack>
       </Stack>
     </>
   )
