aaaf7dec69ee136ef03c939b5409018222a516bf
jruizsilva <ruizsilvajonathan@gmail.com>
2023-12-30 20:13:58 -0300

    search input validations added

--- src/pages/PeoplePage/PeoplePage.tsx
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -1,6 +1,14 @@
-import { useEffect, useState } from 'react'
-import { InputGroup, Input, InputRightElement, Button } from '@chakra-ui/react'
-import toast from 'react-hot-toast'
+import { useEffect } from 'react'
+import {
+  InputGroup,
+  Input,
+  InputRightElement,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  Box
+} from '@chakra-ui/react'
+import { type SubmitHandler, useForm } from 'react-hook-form'
 
 import { ListCardPeople, PeopleContainer } from './components'
 
@@ -9,13 +17,19 @@ import { useUserStore } from 'business/user/useUserStore'
 
 interface Props {}
 
+interface FormValues {
+  peapleToSearch: string
+}
+
 export function PeoplePage(props: Props): JSX.Element {
-  const [peapleToSeach, setPeapleToSeach] = useState('')
   const { fetchAllUsers } = useUser()
   const { users, setUsers } = useUserStore()
-  const handleClick = () => {
-    setPeapleToSeach('test')
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+    getValues
+  } = useForm<FormValues>({ mode: 'onBlur' })
 
   useEffect(() => {
     fetchAllUsers()
@@ -26,15 +40,44 @@ export function PeoplePage(props: Props): JSX.Element {
         console.log(err)
       })
   }, [])
+  const onSubmit: SubmitHandler<FormValues> = async (formValues) => {
+    console.log(formValues)
+  }
 
   return (
     <PeopleContainer>
-      <InputGroup maxWidth={'664px'} mx={'auto'} size='lg'>
-        <Input placeholder='Search people' pr='4.5rem' />
-        <InputRightElement mr={'4px'} width='80px'>
-          <Button onClick={handleClick}>Search</Button>
-        </InputRightElement>
-      </InputGroup>
+      <Box as='form' onSubmit={handleSubmit(onSubmit)}>
+        <FormControl
+          isInvalid={Boolean(errors.peapleToSearch)}
+          maxWidth={'664px'}
+          mx={'auto'}
+        >
+          <InputGroup size='lg'>
+            <Input
+              autoComplete='off'
+              placeholder='Search people'
+              pr='4.5rem'
+              type='text'
+              {...register('peapleToSearch', {
+                minLength: {
+                  value: 2,
+                  message: 'debe tener al menos 2 caracteres'
+                }
+              })}
+            />
+            <InputRightElement mr={'4px'} width='80px'>
+              <Button
+                isDisabled={!isValid || getValues().peapleToSearch?.length < 2}
+                isLoading={isSubmitting}
+                type='submit'
+              >
+                Search
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>{errors.peapleToSearch?.message}</FormErrorMessage>
+        </FormControl>
+      </Box>
       <ListCardPeople users={users} />
     </PeopleContainer>
   )
