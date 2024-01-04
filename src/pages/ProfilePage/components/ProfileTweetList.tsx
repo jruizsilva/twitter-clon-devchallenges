55bdb83ce546771cbf9dcf3bb7d6f5f5e771194a
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-04 17:50:43 -0300

    unused prop removed

--- src/pages/ProfilePage/components/ProfileTweetList.tsx
+++ src/pages/ProfilePage/components/ProfileTweetList.tsx
@@ -3,17 +3,12 @@ import { Box } from '@chakra-ui/react'
 import { TweetCard } from '../../../components/ui/TweetCard'
 
 import { type Post } from 'business/posts/usePost'
-import { type User } from 'business/user/useUser'
 
 interface Props {
   posts: Post[] | null
-  author: User | null
 }
 
-export function ProfileTweetList({
-  posts,
-  author
-}: Readonly<Props>): JSX.Element {
+export function ProfileTweetList({ posts }: Readonly<Props>): JSX.Element {
   return (
     <Box display={'flex'} flexDirection={'column'} gap={'24px'} width={'100%'}>
       {posts != null &&
