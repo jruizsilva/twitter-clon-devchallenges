abc8ad71b8fdf21776eadee258bf398cb98273b4
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-09 19:37:56 -0300

    search on progress

--- src/pages/PeoplePage/PeoplePage.tsx
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -10,10 +10,10 @@ import {
   Center
 } from '@chakra-ui/react'
 import { type SubmitHandler, useForm } from 'react-hook-form'
+import { useRef, useState } from 'react'
 
 import { ListCardPeople, PeopleContainer } from './components'
 
-import { fetchSearchUsersByUsernameOrName } from 'services/user'
 import { useUsersQuery } from 'hooks/queries/userUsersQuery'
 
 interface Props {}
@@ -24,58 +24,65 @@ interface SearchUserRequest {
 
 export function PeoplePage(props: Props): JSX.Element {
   const {
-    register,
     handleSubmit,
-    formState: { errors, isValid, isSubmitting }
+    register,
+    formState: { isSubmitting }
   } = useForm<SearchUserRequest>({ mode: 'onBlur' })
 
   const { users } = useUsersQuery()
+  const [searchResult, setSearchResult] = useState<User[]>([])
+
+  const originalUsers = useRef<User[]>([])
+
+  if (users !== undefined && originalUsers.current.length === 0) {
+    originalUsers.current = users
+    setSearchResult(users)
+  }
 
   const onSubmit: SubmitHandler<SearchUserRequest> = async (formValues) => {
-    if (formValues.peapleToSearch.trim().length === 0) {
+    if (formValues.peapleToSearch.trim().length === 0 || users === undefined) {
+      setSearchResult(originalUsers.current)
+
       return
     }
-    fetchSearchUsersByUsernameOrName(formValues.peapleToSearch)
-      .then((users) => {
-        console.log(users)
+    // :TODO eliminar duplicados
+    const filteredUsers = users.filter((user) => {
+      const result = []
+
+      users.forEach((user) => {
+        result.push(
+          user.name
+            .toLowerCase()
+            .includes(formValues.peapleToSearch.toLowerCase())
+        )
       })
-      .catch((err: unknown) => {
-        console.error(err)
-      })
+
+      return user.name
+        .toLowerCase()
+        .includes(formValues.peapleToSearch.toLowerCase())
+    })
+
+    setSearchResult(filteredUsers)
   }
 
   return (
     <PeopleContainer>
       <Box as='form' onSubmit={handleSubmit(onSubmit)}>
-        <FormControl
-          isInvalid={Boolean(errors.peapleToSearch)}
-          maxWidth={'664px'}
-          mx={'auto'}
-        >
+        <FormControl maxWidth={'664px'} mx={'auto'}>
           <InputGroup size='lg'>
             <Input
               autoComplete='off'
               placeholder='Search people'
               pr='4.5rem'
               type='text'
-              {...register('peapleToSearch', {
-                minLength: {
-                  value: 2,
-                  message: 'debe tener al menos 2 caracteres'
-                }
-              })}
+              {...register('peapleToSearch')}
             />
             <InputRightElement mr={'4px'} width='80px'>
-              <Button
-                isDisabled={!isValid}
-                isLoading={isSubmitting}
-                type='submit'
-              >
+              <Button isLoading={isSubmitting} type='submit'>
                 Search
               </Button>
             </InputRightElement>
           </InputGroup>
-          <FormErrorMessage>{errors.peapleToSearch?.message}</FormErrorMessage>
         </FormControl>
       </Box>
       {isSubmitting ? (
@@ -83,7 +90,7 @@ export function PeoplePage(props: Props): JSX.Element {
           <Spinner />
         </Center>
       ) : (
-        <ListCardPeople users={users} />
+        <ListCardPeople users={searchResult} />
       )}
     </PeopleContainer>
   )
