29d76ba25d90f1ee4e88f4b24f00d2556de3adb8
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-14 13:56:33 -0300

    service edit comment added

--- src/services/comment.ts
+++ src/services/comment.ts
@@ -1,39 +1,52 @@
 import { protectedInstance } from 'api/axiosInstances'
 
-const fetchAddCommentToPost = async (postId: string, commentRequest: CommentRequest) => {
-  const response = await protectedInstance.post<Post>(
-    `/comments/posts/${postId}/addComment`, commentRequest
-  )
-  const post = response.data
+export const fetchAddCommentToPost =
+  async (postId: string, commentRequest: CommentRequest) => {
+    const response = await protectedInstance.post<Post>(
+      `/comments/posts/${postId}/addComment`, commentRequest
+    )
+    const post = response.data
 
-  return post
-}
+    return post
+  }
 
-const fetchDeleteComment = async ({ commentId, postId }: DeleteCommentRequest) => {
-  const response = await protectedInstance.delete<Post>(
-    `/comments/${commentId}/posts/${postId}/removeComment`
-  )
-  const post = response.data
+export const fetchDeleteComment =
+  async ({ commentId, postId }: DeleteCommentRequest) => {
+    const response = await protectedInstance.delete<Post>(
+      `/comments/${commentId}/posts/${postId}/removeComment`
+    )
+    const post = response.data
 
-  return post
-}
+    return post
+  }
 
-const fetchLikeComment = async ({ commentId, postId }: LikeRequest) => {
-  const response = await protectedInstance.patch<Post>(
-    `/comments/${commentId}/posts/${postId}/likeComment`
-  )
-  const post = response.data
+export const fetchLikeComment =
+  async ({ commentId, postId }: LikeRequest) => {
+    const response = await protectedInstance.patch<Post>(
+      `/comments/${commentId}/posts/${postId}/likeComment`
+    )
+    const post = response.data
 
-  return post
-}
+    return post
+  }
 
-const fetchRemoveLikeComment = async ({ commentId, postId }: LikeRequest) => {
-  const response = await protectedInstance.patch<Post>(
-    `/comments/${commentId}/posts/${postId}/removeLikeComment`
-  )
-  const post = response.data
+export const fetchRemoveLikeComment =
+  async ({ commentId, postId }: LikeRequest) => {
+    const response = await protectedInstance.patch<Post>(
+      `/comments/${commentId}/posts/${postId}/removeLikeComment`
+    )
+    const post = response.data
 
-  return post
-}
+    return post
+  }
 
-export { fetchAddCommentToPost, fetchDeleteComment, fetchLikeComment, fetchRemoveLikeComment }
+export const fetchEditComment =
+  async (commentId: string, commentRequest: Required<CommentRequest>) => {
+    const { postId } = commentRequest
+    const response = await protectedInstance.patch<Post>(
+      `/comments/${commentId}/posts/${postId}/editComment`, commentRequest
+    )
+    const post = response.data
+
+    return post
+  }
